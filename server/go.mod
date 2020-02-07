5985864b837e82609d13ed865066aaf1f1ca6bfb
Takamichi Omori <merrytakamerry@gmail.com>
2020-02-07 10:59:34 +0900

    Revert "create databaase connection"
    This reverts commit ba256927e9a6970e69c46c4b1399786cc4574d06.

--- server/go.mod
+++ server/go.mod
@@ -1,5 +1,3 @@
 module main
 
 go 1.13
-
-require github.com/go-sql-driver/mysql v1.5.0
--- server/main.go
+++ server/main.go
@@ -1,15 +1,11 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
-	_ "github.com/go-sql-driver/mysql"
 	"main/handler"
 	"net/http"
 )
 
 func main() {
-	checkSQLConnection()
 	http.HandleFunc("/", handler.IndexHandler)
 
 	//user
@@ -19,38 +15,4 @@ func main() {
 
 	// 8080ポートで起動
 	http.ListenAndServe(":8080", nil)
-}
-
-func checkSQLConnection() {
-	db, err := sql.Open("mysql", "test:password@tcp(127.0.0.1:3306)/test")
-	if err != nil {
-		panic(err)
-	}
-
-	err = db.Ping()
-	if err != nil {
-		panic(err)
-	}
-	defer db.Close()
-
-	var id int
-	var name string
-	rows, err := db.Query("select * from users where id < ?", 6)
-	if err != nil {
-		panic(err)
-	}
-	defer rows.Close()
-
-	for rows.Next() {
-		err := rows.Scan(&id, &name)
-		if err != nil {
-			panic(err)
-		}
-		fmt.Println(id, name)
-	}
-
-	err = rows.Err()
-	if err != nil {
-		panic(err)
-	}
 }
